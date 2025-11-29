c7caa2c4642468664b1b8b73f169aae1fd88a010
شهد نشأت صلاح أحمد stu <324243152@sha.edu.eg>
2025-11-30 00:09:28 +0200

    Increase JSON and URL-encoded request size limits in server configuration

--- src/server.js
+++ src/server.js
@@ -20,7 +20,8 @@ const io = new IOServer(server, {
 });
 
 // Middlewares
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
 
 // Logger
